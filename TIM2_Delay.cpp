cbf4b40fae328699c2efb2ac60823c9409b6d736
That_Electronics_Guy <chris@medisource.tech>
2026-04-11 17:33:42 -0400

    Update TIM2_Delay.cpp

--- TIM2_Delay.cpp
+++ TIM2_Delay.cpp
@@ -8,7 +8,7 @@ int main(void)
 	RCC->AHB1ENR |= 1; /* enable GPIOA clock */
 	RCC -> APB1ENR |= 1; /* Enable CLK source to TIM2 */
 	TIM2 -> PSC = 1600 - 1; 
-	TIM2 -> ARR = 10000;
+	TIM2 -> ARR = 10000 - 1;
 	TIM2 -> CNT = 0;
 	TIM2 -> CR1 = 1; /*Enable Timer2 with an UP Counter */
 	GPIOA->MODER &= ~0x00000C00; /* clear pin mode */
